e78b4e1eadf448c685da02dcfdf5da87d7526a11
Andrii Ostapenko <droid85@yahoo.com>
2022-02-17 23:36:14 +0200

    created public directory added README.md file

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const { application } = require('express');
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
-app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/index.html`)
+app.get('/public', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
 })
 
-app.get('/assets/about/', (req, res) => {
-    res.sendFile(`${__dirname}/assets/about/about.html`)
+app.get('/public/assets/about', (req, res) => {
+    res.sendFile(`${__dirname}/public/assets/about/about.html`)
+})
+
+app.get('/public/assets/averages/', (req, res) => {
+    res.sendFile(`${__dirname}/public/assets/averages/averages.html`)
 })
 
 app.listen(PORT, () => {
