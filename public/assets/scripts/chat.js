f04b7953801878a9ba16b448862601adb10e1433
Andrii Ostapenko <droid85@yahoo.com>
2022-02-22 22:47:49 +0200

    compleated chat page with client size render

--- public/assets/scripts/chat.js
+++ public/assets/scripts/chat.js
@@ -3,39 +3,58 @@ const chatInfo = [
     name: "Leonardo",
     msg: "Hello",
     data: "12.03.2020 12:30",
-    color: "#111322",
     },
     {
     name: "Raphael",
     msg: "Hi",
     data: "12.03.2020 12:34",
-    color: "#222322",
     },
     {
     name: "Michelangelo",
     msg: "WhatsUp",
     data: "12.03.2020 13:00",
-    color: "#333322",
     },
     {
     name: "Donatello",
     msg: "Hello everyone",
     data: "12.03.2020 13:01",
-    color: "#444322",
     },
 ]
 
-const chatSectionEl = document.querySelector('$chatSection');
+const chatSectionEl = document.querySelector('#chatSection');
 const textEl = document.querySelector('#textMessage');
 const inputEl = document.querySelector('#inputMessage');
 const sendBtnEl = document.querySelector('#chatBtn');
+const msgsEl = document.querySelector('#chatMsgs');
 
 sendBtnEl.addEventListener('click', onClickBtn);
 
-function onClickBtn(e) {
-    console.log(e.target)
+for (let i = 0; i < chatInfo.length; i++) {
+    renderMsgs(chatInfo[i])
 }
 
-module.exports = {
-    onClickBtn
+function onClickBtn(e) {
+    const curentMas = {
+        name: inputEl.value,
+        msg: textEl.value,
+        data: new Date().toLocaleDateString('en-US')
+    }
+    chatInfo.push(curentMas)
+    renderMsgs(curentMas)
+    clearForm(textEl, inputEl);
+}
+
+function renderMsgs(obj) {
+    return msgsEl.insertAdjacentHTML('beforeend', `
+        <div class="msg-block">
+            <div class="msg-name">${obj["name"]}</div>
+            <p class="msg-text">${obj["msg"]}</p>
+            <div  class="msg-date">${obj["data"]}</div>
+        </div>
+    `)
+}
+
+function clearForm(textArea, input) {
+    textArea.value = '';
+    input.value = '';
 }
